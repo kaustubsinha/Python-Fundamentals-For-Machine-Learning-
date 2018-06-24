1b8d4e08f001b0ae371fccf90dcef8eca860bf07
Kaustub Sinha <kaustubsinha@gmail.com>
2018-06-24 23:44:14 +0530

    Add files via upload

---
+++ 8. Order Dictionary, Counter, Default Dictionary/Odr Dict, Default Dict, Conters.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 13 15:06:17 2018
+
+@author: Kaustub Sinha
+"""
+"""
+Q1. (Create a program that fulfills the following specification.)
+"""
+
+#namelist = []
+#
+#while True:
+#    str1 = input("Enter the data: ")
+#    if str1 == '':
+#        break
+#    tup1 = str1.split(",")
+#    tup2 = (tup1[0], int(tup1[1]), int(tup1[2]))
+#    namelist.append(tup2)
+#    
+#namelist.sort()
+#    
+#print (namelist)
+    
+
+"""
+Q2. (Create a program that fulfills the following specification.)
+12 9 -61 5 14 121
+"""
+#from collections import OrderedDict
+#
+#
+#item_name = OrderedDict()
+#item_name["BANANA FRIES"]=12
+#item_name["POTATO CHIPS"]=30
+#item_name["APPLE JUICE"]=10
+#item_name["CANDY"]=5
+#item_name["APPLE JUICE"]= item_name["APPLE JUICE"] + 10
+#item_name["CANDY"]= item_name["CANDY"] + 5
+#item_name["CANDY"]= item_name["CANDY"] + 5
+#item_name["CANDY"]= item_name["CANDY"] + 6
+#item_name["POTATO CHIPS"]= item_name["POTATO CHIPS"] + 30
+
+#from collections import OrderedDict
+#odict=OrderedDict()
+#while True:
+#    item=input ().split(" ")
+#    if item ==['']:
+#        break
+#    key = ' '.join(item[:-1])
+#    val = int(item[-1])
+#    odict[key]=odict.get(key,0)+val
+#
+#
+#for k,v in odict.items():
+#    print (k, v])
+
+"""
+Q3. (Create a program that fulfills the following specification.)
+
+"""
+#list1 = input("Enter :").split(" ")
+#if all([all(int(i)>0 for i in list1),any(i==i[::-1] for i in list1)]):
+#    print (True)
+#else:
+#    print (False)    
+    
+
+
+
+
+
+
+
+
+
